994c1a2cb21ae4cbd3cf7866caa8b74c76f54375
Gabriel Leiva <gaboleiva@192.168.1.7>
2024-05-13 20:27:39 -0400

    Levantamos servidor express

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 const express = require('express');
 const app = express();
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo por el puerto ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
