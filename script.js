6d091a3f6da8914180a079755ba2525bc8c5140e
Gabriel Leiva <gaboleiva@192.168.1.7>
2024-05-14 14:06:13 -0400

    Rutas eliminar gasto, editar gasto resueltas

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import fs from 'fs';
+import { v4 as uuidv4 } from 'uuid';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -18,9 +19,11 @@ app.listen(PORT, () => {
     console.log(`Servidor corriendo por el puerto ${PORT}`);
 });
 
+// Importamos los módulos que vamos a utilizar
 import { agregarRoommate } from './modulos/roommates.js';
 // import { agregarGasto } from './modulos/gastos.js';
 
+// Middlewares
 app.use(express.json());
 
 // ruta raíz que devuelve con sendFile el archivo index.html
@@ -28,6 +31,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+// ruta para agregar un roommate al archivo roommates.json 
 app.post('/roommate', async (req, res) => {
     try {
         // Verificamos que el archivo roommates.json existe, si no existe lo creamos con un arreglo vacío
@@ -41,21 +45,25 @@ app.post('/roommate', async (req, res) => {
     }
 });
 
+// ruta para obtener todos los roommates del archivo roommates.json
 app.get('/roommates', (req, res) => {
     const { roommates } = JSON.parse(fs.readFileSync("roommates.json", "utf8"));
     res.send(roommates);
 });
 
+// ruta para agregar un gasto al archivo gastos.json
 app.post('/gasto', async (req, res) => {
     try {
         // Verificamos que el archivo gastos.json existe, si no existe lo creamos con un arreglo vacío
         if (!fs.existsSync("gastos.json")) {
             fs.writeFileSync('gastos.json', '{"gastos": []}', 'utf8');
         };
+        const idGasto = uuidv4().slice(24);
         // const result = await agregarGasto();
-        console.log("Esto es req.body: ", req.body);
+        // console.log("Esto es req.body: ", req.body);
         const { nombre, comentario, monto } = req.body;
         const gasto = {
+            id: idGasto,
             nombre,
             comentario,
             monto,
@@ -66,16 +74,47 @@ app.post('/gasto', async (req, res) => {
         fs.writeFileSync("gastos.json", JSON.stringify({ gastos }));
         res.send("Gasto agregado con existo!!");
     } catch (error) {
-        console.log("Como que no existe?");
         res.status(500).send(error.message);
     }
 });
 
+// ruta para obtener todos los gastos del archivo gastos.json
 app.get('/gastos', (req, res) => {
     const { gastos } = JSON.parse(fs.readFileSync("gastos.json", "utf8"));
     res.send(gastos);
 });
 
+// ruta para ditar un gasto del archivo gastos.json
+app.put('/gasto', async (req, res) => {
+    try {
+    const { id, nombre, comentario, monto } = req.body;
+    console.log("El valor de req.params es: ", req.body);
+    const gasto = { id, nombre, comentario, monto  };
+    const gastosJSON = JSON.parse(fs.readFileSync("gastos.json", "utf8"));
+    const gastos = gastosJSON.gastos;
+    gastosJSON.gastos = gastos.map((b) => b.id === id? gasto : b);
+    fs.writeFileSync("gastos.json", JSON.stringify(gastosJSON));
+    res.send("Gasto actualizado con éxito!!");
+    } catch (error) {
+        res.status(500).send(error.message);
+        console.log("BARRANCO!!!!!!!!");
+    }
+    
+});
+
+// ruta para eliminar un gasto del archivo gastos.json
+app.delete('/gasto', async (req, res) => {
+    try {
+        const { id } = req.query;
+        const gastosJSON = JSON.parse(fs.readFileSync("gastos.json", "utf8"));
+        const gastos = gastosJSON.gastos;
+        gastosJSON.gastos = gastos.filter((b) => b.id !== id);
+        fs.writeFileSync("gastos.json", JSON.stringify(gastosJSON));
+        res.send("Gasto eliminado con éxito!!");
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
 // ruta generica
 app.get("*", (req, res) => {
     res.send("Esta página no existe!!");
